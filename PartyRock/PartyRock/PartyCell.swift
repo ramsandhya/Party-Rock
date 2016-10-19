fbf248e210fc1df25193ae6e8b1fda18f1beb8b9
ramsandhya <ram.sandhya@gmail.com>
2016-10-19 13:49:27 -0400

    COnfigured the custom cell and model

---
+++ PartyRock/PartyRock/PartyCell.swift
@@ -0,0 +1,28 @@
+//
+//  PartyCell.swift
+//  PartyRock
+//
+//  Created by Jitendra Ram on 10/18/16.
+//  Copyright © 2016 Sandhya Ram. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    func updateUI() {
+        
+    }
+
+}
