f9d3bd0737eb4c5f6c0e07b2044d22de9091d74f
ramsandhya <ram.sandhya@gmail.com>
2016-10-18 22:16:59 -0400

    Initial tableView UI setup

---
+++ PartyRock/PartyRock/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by Jitendra Ram on 10/18/16.
+//  Copyright © 2016 Sandhya Ram. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
